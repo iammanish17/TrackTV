907808202ceacab79f1d6f602b17de98e11edc86
manish17 <35730135+iammanish17@users.noreply.github.com>
2020-06-02 20:28:56 +0530

    Make migrations.

--- tv/models.py
+++ tv/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Show(models.Model):
+    showid = models.IntegerField()
+    plot = models.TextField()
+    status = models.CharField(max_length=32)
+
+
+class UserRating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    show = models.ForeignKey(Show, on_delete=models.CASCADE)
+    rating = models.IntegerField()
